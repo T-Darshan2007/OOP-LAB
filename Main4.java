04f9b23831aa92f6187591ea01e337ebcece84a0
T DARSHAN <tdarshan2007@gmail.com>
2026-04-14 17:16:37 +0530

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();  // inherited method
+        d.bark();   // own method
+    }
+}
