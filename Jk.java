0b7b8bce76e8909a61f781a2783ebe22d20e7814
T DARSHAN <tdarshan2007@gmail.com>
2026-04-22 13:06:09 +0530

    Add files via upload

---
+++ Jk.java
@@ -0,0 +1,17 @@
+public class Jk{
+    public static void main(String[] args) {
+        int num = 2;
+        switch (num) {
+            case 1:
+                System.out.println("No 1");
+                
+                break;
+            case 2:
+                System.out.println("No 2");
+                break;
+                
+            default:
+                System.out.println("Another");
+        }
+    }
+}
