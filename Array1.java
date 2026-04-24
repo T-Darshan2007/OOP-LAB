056a2215b1092b5ec06c52d7681e41ff98688f69
T DARSHAN <tdarshan2007@gmail.com>
2026-04-24 18:07:55 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,11 @@
+
+public class Array1{
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+        int len = arr.length;
+
+        for(int i = 0; i<len; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
