3d84ea76f22f7ed12bdcec026fd048ed0a1180e6
T DARSHAN <tdarshan2007@gmail.com>
2026-04-24 18:04:00 +0530

    Add files via upload

---
+++ Sm1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Sm1{
+    public static void main(String[] args) {
+        String[] arr={"Ajith", "Shalini", "Kadavul"};
+        System.out.println(Arrays.toString(arr));
+    }
+}
