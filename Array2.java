b7aab5442470a4b56d6eadd8be0fd629ce63a063
T DARSHAN <tdarshan2007@gmail.com>
2026-04-25 19:57:01 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Array2{
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(78);
+        arr.add(89);
+        arr.add(88);
+        System.out.println(arr);
+    }
+}
