99320527ab90d875896896f5410348ae147e40eb
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-08 18:29:44 +0530

    Add files via upload

---
+++ Pat.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pat {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int num = inp.nextInt();
+
+        for (int i = 1; i < num+1; i++) {
+            for (int j = 1; j < i+1; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
