a23304cb88497f436c599a4962267e4f69d004ce
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-12 21:23:14 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,12 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 18;
+
+        if (a > b) {
+            System.out.println(a + " is largest");
+        } else {
+            System.out.println(b + " is largest");
+        }
+    }
+}
