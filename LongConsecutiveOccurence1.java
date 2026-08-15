d9a00dfb42cdda060df41007d9c6548399138030
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-15 19:35:15 +0530

    Add files via upload

---
+++ LongConsecutiveOccurence1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class LongConsecutiveOccurence1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        String str = inp.nextLine();
+
+        int count = 0, temp = -999;
+        char c = ' ';
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char at = str.charAt(i);
+
+            for (int j = 0; j < str.length(); j++) {
+                char At = str.charAt(j);
+
+                if (at == At) {
+                    count++;
+                }
+            }
+
+            if (count > temp) {
+                temp = count;
+                count = 0;
+                c = at;
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        System.out.println(c + " " + temp);
+    }
+}
