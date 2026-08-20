65b2d759048480b3b1f451673c6a70771ce53964
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-20 21:35:49 +0530

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
 public class EvenOdd {
     public static void main(String[] args) {
-        int n = 10;
-
-        if (n % 2 == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("Odd");
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a integer number:-");
+        int num = inp.nextInt();
+        if(num%2==0) {
+            System.out.println("Even number;");
+        }
+        else {
+            System.out.println("Odd number;");
         }
     }
 }
