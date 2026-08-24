7a20dcbdefc2458710db7eef6b66a6d63ba1da5a
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-24 21:23:52 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Sample {
+    public static void main(String[] args) {
+        ArrayList<Integer> Arr = new ArrayList<>();
+        Arr.add(10);
+        Arr.add(20);
+        Arr.add(30);
+        System.out.println(Arr);
+    }
+}
