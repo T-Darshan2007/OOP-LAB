56ff5b0d8e95fbc0617e08717c3ad1c8c44d1909
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-24 21:23:28 +0530

    Add files via upload

---
+++ Mehods.java
@@ -0,0 +1,23 @@
+public class Mehods {
+    static void sum(int a, int b) {
+        System.out.println("Sum = " + (a+b));
+    }
+
+    static void sub(int a, int b) {
+        System.out.println("Sub = " + (a-b));
+    }
+
+    static void mul(int a, int b) {
+        System.out.println("Mul = " + (a*b));
+    }
+
+    static void div(int a, int b) {
+        System.out.println("Mul = " + (a/b));
+    }
+    public static void main(String[] args) {
+        sum(25,5);
+        sub(25,5);
+        mul(25,5);
+        div(25,5);
+    }
+}
