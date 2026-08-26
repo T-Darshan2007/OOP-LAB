f437e5884d5e914cd6bf801e3f85c06160969a27
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-26 19:18:01 +0530

    Add files via upload

---
+++ SON.java
@@ -0,0 +1,7 @@
+public class SON {
+    public static void main(String[] args) {
+        int n = 10, sum = 0;
+        for (int i = 1; i <= n; i++) sum += i;
+        System.out.println("Sum = " + sum);
+    }
+}
