82bdb16da55d0943f99c6f67825fdfc746900d01
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-26 19:17:35 +0530

    Add files via upload

---
+++ EO.java
@@ -0,0 +1,7 @@
+public class EO {
+    public static void main(String[] args) {
+        int n = 24;
+        if (n % 2 == 0) System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
