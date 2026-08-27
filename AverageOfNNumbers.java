e32c87bab725fdff971781d1b1bb795725719647
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-27 21:02:14 +0530

    Add files via upload

---
+++ AverageOfNNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AverageOfNNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += sc.nextDouble();
+        System.out.println("Average = " + (sum / n));
+    }
+}
