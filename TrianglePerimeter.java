5de19af600c64f554f5ba4af29215f22372bed70
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:32:33 +0530

    Add files via upload

---
+++ TrianglePerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class TrianglePerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double a=sc.nextDouble(), b=sc.nextDouble(), c=sc.nextDouble(); System.out.println("Perimeter of Triangle = " + (a+b+c)); } }
