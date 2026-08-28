f1ee11a40af9ed039b8e99c810a80a1896a866e5
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:30:22 +0530

    Add files via upload

---
+++ TrapeziumPerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class TrapeziumPerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double a=sc.nextDouble(), b=sc.nextDouble(), c=sc.nextDouble(), d=sc.nextDouble(); System.out.println("Perimeter of Trapezium = " + (a+b+c+d)); } }
