bb1fedc210222a5eeb3c209e995dd4d585724f2f
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:31:20 +0530

    Add files via upload

---
+++ RectanglePerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class RectanglePerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double l=sc.nextDouble(), w=sc.nextDouble(); System.out.println("Perimeter of Rectangle = " + (2*(l+w))); } }
