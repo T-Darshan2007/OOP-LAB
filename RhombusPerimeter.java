2c81ce71e0947c38a0978f1e323cf0f395a3645b
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:33:43 +0530

    Add files via upload

---
+++ RhombusPerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class RhombusPerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double s=sc.nextDouble(); System.out.println("Perimeter of Rhombus = " + (4*s)); } }
