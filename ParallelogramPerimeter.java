230ba44d00da9468e3f8da71b292795753f11a1a
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:33:10 +0530

    Add files via upload

---
+++ ParallelogramPerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class ParallelogramPerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double b=sc.nextDouble(), s=sc.nextDouble(); System.out.println("Perimeter of Parallelogram = " + (2*(b+s))); } }
