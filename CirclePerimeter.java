d1691c609a978d98708eac286251047fd8e91bf4
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-28 21:30:49 +0530

    Add files via upload

---
+++ CirclePerimeter.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class CirclePerimeter { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double r=sc.nextDouble(); System.out.println("Perimeter of Circle = " + (2*Math.PI*r)); } }
