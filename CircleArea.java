a75c4b0afccc8385ff6a41db88c62dec61f0fe2e
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:14:53 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class CircleArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double r=sc.nextDouble(); System.out.println("Area of Circle = "+(Math.PI*r*r)); } }
