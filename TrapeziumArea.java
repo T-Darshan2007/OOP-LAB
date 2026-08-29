648150d3956f075ffe113011976d22396749de81
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:18:17 +0530

    Add files via upload

---
+++ TrapeziumArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class TrapeziumArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double a=sc.nextDouble(), b=sc.nextDouble(), h=sc.nextDouble(); System.out.println("Area of Trapezium = "+(0.5*(a+b)*h)); } }
