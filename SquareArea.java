2e89385db0260efab0fd93a419dc7d664acc93ea
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:16:07 +0530

    Add files via upload

---
+++ SquareArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class SquareArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double s=sc.nextDouble(); System.out.println("Area of Square = "+(s*s)); } }
