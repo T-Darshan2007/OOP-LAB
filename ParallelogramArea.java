381e0caf1a44918fbe437da9fe5d09ff894a6ed6
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:17:13 +0530

    Add files via upload

---
+++ ParallelogramArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class ParallelogramArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double b=sc.nextDouble(), h=sc.nextDouble(); System.out.println("Area of Parallelogram = "+(b*h)); } }
