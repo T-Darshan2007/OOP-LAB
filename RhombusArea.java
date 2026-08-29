d8041c738986744b399beb78f5e485f63426e0bb
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:17:43 +0530

    Add files via upload

---
+++ RhombusArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class RhombusArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double d1=sc.nextDouble(), d2=sc.nextDouble(); System.out.println("Area of Rhombus = "+(0.5*d1*d2)); } }
