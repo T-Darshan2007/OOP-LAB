d3ff04c598a8c42c66a577f234ba2633980d8c5b
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:15:32 +0530

    Add files via upload

---
+++ RectangleArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class RectangleArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double l=sc.nextDouble(), w=sc.nextDouble(); System.out.println("Area of Rectangle = "+(l*w)); } }
