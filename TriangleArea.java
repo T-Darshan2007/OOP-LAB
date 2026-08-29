c06fbbef2e28f303f78bb5138a8ee5a04d26d281
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-29 21:16:38 +0530

    Add files via upload

---
+++ TriangleArea.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class TriangleArea { public static void main(String[] args) { Scanner sc=new Scanner(System.in); double b=sc.nextDouble(), h=sc.nextDouble(); System.out.println("Area of Triangle = "+(0.5*b*h)); } }
