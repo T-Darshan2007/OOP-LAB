aaa6f70f8cdb813637519d66b2cb5f72a19be1cf
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-30 17:10:44 +0530

    Add files via upload

---
+++ MatrixMultiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[2][2], b = new int[2][2], c = new int[2][2];
+        for (int i=0;i<2;i++) for (int j=0;j<2;j++) a[i][j]=sc.nextInt();
+        for (int i=0;i<2;i++) for (int j=0;j<2;j++) b[i][j]=sc.nextInt();
+        for (int i=0;i<2;i++) for (int j=0;j<2;j++)
+            for (int k=0;k<2;k++) c[i][j]+=a[i][k]*b[k][j];
+        for (int i=0;i<2;i++) { for (int j=0;j<2;j++) System.out.print(c[i][j]+" "); System.out.println(); }
+    }
+}
