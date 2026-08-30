c94b3e3e0b8af410c1752f072348b6da4ffc0d1c
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-08-30 17:09:19 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int vowels=0, consonants=0;
+        for (int i=0;i<str.length();i++) {
+            char c=str.charAt(i);
+            if (c>='a' && c<='z') {
+                if ("aeiou".indexOf(c)>=0) vowels++;
+                else consonants++;
+            }
+        }
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+}
