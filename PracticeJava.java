eeb8068bbc6b54b45646d69c380b29d56bafeff8
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-16 21:09:45 +0530

    Add files via upload

---
+++ PracticeJava.java
@@ -0,0 +1,18 @@
+public class PracticeJava {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a,b);
+        System.out.println(a +" "+ b);
+    }
+
+    static void swap(int a, int b){
+        int c = a;
+        a = b;
+        b = c;
+    }
+
+    static void func() {
+        System.out.println();
+    }
+}
