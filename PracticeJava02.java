77c011974cb55db924bfd2f9080835c249ad4560
T DARSHAN <252712198+T-Darshan2007@users.noreply.github.com>
2026-09-19 21:29:38 +0530

    Add files via upload

---
+++ PracticeJava02.java
@@ -0,0 +1,8 @@
+public class PracticeJava02 {
+    public static void main(String[] args) {
+        int[] arr = {10,78,67,56,55,40,100};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]*2 + " ");
+        }
+    }
+}
